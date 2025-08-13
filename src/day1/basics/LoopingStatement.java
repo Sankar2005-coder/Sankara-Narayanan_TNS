d8482df71d0f74be77bef47c86cafdbc87a69f5c
Sankar2005-coder <sankaranarayanan212005@gmail.com>
2025-08-13 09:03:37 +0530

    commit4

---
+++ src/day1/basics/LoopingStatement.java
@@ -0,0 +1,11 @@
+package day1.basics;
+
+public class LoopingStatement {
+	  public static void main(String[] args) {
+		  for(int i = 1; i<=1000 ; i++)   //(int i = 1000; i<=1;i--)
+			{	
+				System.out.print("Value of i:");
+				System.out.println(i);
+			}
+		}
+}
